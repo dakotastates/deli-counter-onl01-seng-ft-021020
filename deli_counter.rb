e72d79b1900dc20dbbee4c59d66bb0234f0bd7ba
Dakota States <dakota.states@gmail.com>
2020-02-12 19:56:56 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,1 +1,20 @@
-# Write your code here.
+
+
+def line(katz_deli)
+  if katz_deli.length == 0
+    puts "The line is currently empty."
+end
+end
+
+def take_a_number(katz_deli, name)
+  katz_deli.each_with_index {|name, index| puts "The line is currently: #{index +1}. #{name}"}
+end
+
+def now_serving(katz_deli)
+  if katz_deli.length >= 1
+    katz_deli.shift
+  else
+  puts "There is nobody waiting to be served"
+  
+end
+end
